6f1967281433cc0b4cd9d8eb516a1adb7acc5b26
Cristina Irene Fortiz <cfortiz1@jhu.edu>
2025-01-14 15:13:14 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <AppBar position="static">
           <Toolbar>
             <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Celebrity Haters- Reddit Analysis Dashboard
+              Celebrity "Haters" Analysis
             </Typography>
             <Button color="inherit" component={Link} to="/">Home</Button>
             <Button color="inherit" component={Link} to="/about">About</Button>
